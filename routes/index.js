5d76fd70d08384c78b519ae34c4f37793b4d70a3
Naveen <naveen.w3master@gmail.com>
2019-03-28 00:07:52 +0530

    Login & Register MongoDB

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var db = require('./../config/db');
 
+const MongoClient = require('mongodb').MongoClient;
+var dbo;
+MongoClient.connect('mongodb://localhost:27017/', (err, db) => {
+  if (err) throw err;
+  dbo = db.db("socialmedia");
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -12,17 +19,19 @@ router.post('/login', function(req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
   var status = "ACTIVE";
-  db.query('SELECT * FROM users WHERE Email = ? AND Password = ? AND Status = ?', [email, password, status], function(err, chkresult) {
-    if (err) throw err;
 
+  dbo.collection("users").find({email: email, password: password, status: status}).toArray(function(err, chkresult){
+    if (err) throw err;
     if(chkresult.length > 0){
-      req.session.uid = chkresult[0].UserID;
-      req.session.name = chkresult[0].Name;
+      console.log(chkresult);
+      req.session.uid = chkresult[0]._id;
+      req.session.name = chkresult[0].name;
       res.redirect('/posts');
     } else {
       res.redirect('/');
     }
   });
+
 });
 
 /* POST register page. */
@@ -31,13 +40,14 @@ router.post('/register', function(req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
   var status = "ACTIVE";
-  db.query('SELECT * FROM users WHERE Email = ?', [email], function(err, chkresult) {
-    if (err) throw err;
 
+  dbo.collection("users").find({email: email}).toArray(function(err, chkresult){
+    if (err) throw err;
     if(chkresult.length == 0){
-      db.query('INSERT INTO users (Name, Email, Password, Status) VALUES (?, ?, ?, ?)', [name, email, password, status], function(err, result) {
+      var userobj = { name: name, email: email, password: password, status: status };
+      dbo.collection("users").insertOne(userobj, function(err, result) {
         if (err) throw err;
-        req.session.uid = result.insertId;
+        req.session.uid = result.insertedId;
         req.session.name = name;
         res.redirect('/posts');
       });
@@ -45,6 +55,7 @@ router.post('/register', function(req, res, next) {
       res.redirect('/');
     }
   });
+
 });
 
 /* GET register page. */
